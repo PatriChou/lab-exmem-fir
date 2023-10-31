71afd92e251883d68f3a72b2ead4681fcad6ded9
Patrick <145217252+PatriChou@users.noreply.github.com>
2023-10-31 21:56:55 +0800

    Add files via upload

---
+++ testbench/counter_la_fir/fir.c
@@ -0,0 +1,26 @@
+#include "fir.h"
+
+void __attribute__ ( ( section ( ".mprjram" ) ) ) initfir() {
+	//initial your fir
+	int i;
+
+	for(i=0; i<N; i++){
+		inputbuffer[i] 	= 0;
+		outputsignal[i] = 0;
+	}
+}
+
+int* __attribute__ ( ( section ( ".mprjram" ) ) ) fir(){
+	initfir();
+	//write down your fir
+	int i, j;
+
+	for(i=0; i<N; i++){
+		for(j=0; j<=i; j++){
+			outputsignal[i] = outputsignal[i] + taps[j] * inputsignal[j];
+		}
+	}
+
+	return outputsignal;
+}
+		
